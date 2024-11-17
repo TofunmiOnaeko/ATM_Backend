03a546b27bcdc84502e486bdd759c7a35a7cfcfb
Tof Onaeko <tonaeko@hotmail.co.uk>
2024-11-17 21:58:24 +0000

    ATM1: Created spring applicatin and made the project a maven project

---
+++ src/main/java/atm/Application.java
@@ -0,0 +1,13 @@
+package atm;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+//        SpringApplication.run(Application.class, args);
+    }
+
+}
